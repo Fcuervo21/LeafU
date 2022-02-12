9d6f1c6b75834ceff08c61d625553b7578b96d1e
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-02-11 18:20:59 -0600

    Navbar component done #3

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import BottomNavigation from './navigation/BottomNavigation/BottomNavigation';
-import Home from './screens/Home/Home';
-import Calculator from './screens/Calculator/Calculator';
-import Community from './screens/Community/Community';
-import News from './screens/News/News';
-import Learn from './screens/Learn/Learn';
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import BottomNavigation from "./navigation/BottomNavigation/BottomNavigation";
+import Home from "./screens/Home/Home";
+import Calculator from "./screens/Calculator/Calculator";
+import Community from "./screens/Community/Community";
+import News from "./screens/News/News";
+import Learn from "./screens/Learn/Learn";
 
 const App = () => {
 	return (
@@ -13,11 +13,11 @@ const App = () => {
 			<div>
 				<BottomNavigation />
 				<Switch>
-					<Route exact path ='/' component={Home}/>
-					<Route exact path ='/Calculator' component={Calculator}/>
-					<Route exact path ='/News' component={News}/>
-					<Route exact path ='/Community' component={Community}/>
-					<Route exact path ='/Learn' component={Learn}/>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/Calculator" component={Calculator} />
+					<Route exact path="/News" component={News} />
+					<Route exact path="/Community" component={Community} />
+					<Route exact path="/Learn" component={Learn} />
 					{/* <Route exact path ='/Settings' component={Settings}/> */}
 				</Switch>
 			</div>
---
+++ client/src/screens/Home/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles(() => ({
+	mainContainer: {
+		width: "100%",
+		padding: "2%",
+		height: "100vh",
+	},
+	contentContainer: {
+		marginTop: "80px",
+	},
+}));
