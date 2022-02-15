a877dc5f89c71042c4e138e47c7ce0137b37408d
Jorge Venegas <32798923+JorgeVenegas@users.noreply.github.com>
2022-02-14 19:06:54 -0600

    Revert "Added Home Cards components"
    This reverts commit 720193c33ba8ba5410770e1170e8da660d34ce63.

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,50 +1,6 @@
 import React from "react";
 import useStyles from "./components/styles";
 import { Typography } from "@mui/material";
-import InfoCard from './components/InfoCard/InfoCard';
-import StatisticsCard from './components/StatisticsCard/StatisticsCard';
-import NewsCard from './components/NewsCard/NewsCard';
-import CommunityCard from './components/CommunityCard/CommunityCard';
-
-const cards = () => {
-	let cards = [];
-	while (true) {
-		let card;
-		let randCard;
-		let randType;
-		let randColor;
-		switch (randCard) {
-			case 1:
-				{
-					card = <StatisticsCard
-						type={ }
-						color={ }
-					/>
-					break;
-				}
-			case 1:
-				{
-					card = <NewsCard
-						type={ }
-						color={ }
-					/>
-					break;
-				}
-			case 1:
-				{
-					<CommunityCard
-						type={ }
-						color={ }
-					/>
-					break;
-				}
-			default:
-				{
-					break;
-				}
-		};
-	};
-};
 
 const Home = () => {
 	const classes = useStyles();
@@ -54,16 +10,6 @@ const Home = () => {
 				<Typography variant="h4" color="initial">
 					Home
 				</Typography>
-				<InfoCard
-					type={ }
-					color={ }
-					
-				/>
-				<StatisticsCard
-					type={ }
-					color={ }
-				/>
-				{ cards }
 			</div>
 		</>
 	);
