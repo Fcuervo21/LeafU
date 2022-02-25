d8cf94a9faa2f765df5f3e04f271683cb8560ae4
Fcuervo21 <fernando.cuervola@udlap.mx>
2022-02-24 22:53:12 -0600

    Firebase setup #19

---
+++ server/database/firebase.js
@@ -0,0 +1,11 @@
+// import dotenv from 'dotenv';
+// import { initializeApp } from 'firebase-admin/app';
+// import { getFirestore, collection, getDocs } from 'firebase/firestore';
+// dotenv.config();
+
+// initializeApp({
+//     credential: applicationDefault(),
+// });
+
+// export const db = getFirestore();
+
